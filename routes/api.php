9ad9ab3bb791bc2f0cd7e1e4aa7187b4f89ec135
danilo alves lopes <daniloalveslopesdev2023@gmail.com>
2025-03-14 15:57:01 -0300

    implementação das rotas e getCurrentUser

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout')->middleware('auth:api');
+    Route::post('refresh', 'refresh')->middleware('auth:api');
+
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('user', 'getCurrentUser');
+})->middleware('auth:api');
